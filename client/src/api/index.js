10b5249e383b287fbe572aab4a7ed97ea623f0d2
Daniel Payne <42071158+ReadyProgrammer1@users.noreply.github.com>
2018-10-11 18:28:39 -0400

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,6 @@
 import fetch from 'isomorphic-fetch'
 require('es6-promise').polyfill()
 
-//fix header being created twice from createUserEntry(values).then(json => { in create.jsx
 export default function api(method, path, data) {
   return fetch(`//localhost:3001/${path}`, {
     method: method.toUpperCase(),
@@ -12,13 +11,5 @@ export default function api(method, path, data) {
       'Access-Control-Allow-Origin': window.location.origin,
     }),
     body: JSON.stringify(data)
-  //}).then(response => response.json())
-  }).then(function(response) {
-    var contentType = response.headers.get("content-type")
-    if(contentType && contentType.includes("application/json")) {
-      return response.json()
-    }
-    throw new TypeError("Oops, we haven't got JSON!");
-  })
-  .catch(function(error) { console.log(error); })
+  }).then(response => response.json())
 }
